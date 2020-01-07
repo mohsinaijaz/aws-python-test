521d941637a161e3e2b23d3b678dfab87dd7a63c
Mohsin Aijaz <mohsin@Mohsins-MacBook-Pro.local>
2020-01-06 21:57:33 -0500

    adding eni

--- ec2.py
+++ ec2.py
@@ -194,7 +194,7 @@ def create_dynamic_instance(random_variable=None):
         #NetworkInterfaces=eni,
         #EbsOptimized=True,
         NetworkInterfaces=[
-        {   'DeviceIndex': '0,'
+        {   'DeviceIndex': '0',
             'NetworkInterfaceId': 'eni-0331c5e11c70144db',
             'SubnetId': 'sg-c832efba'
         },
